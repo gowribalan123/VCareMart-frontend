ecb1aa03ba48511241fd2ab21d96a82850352a42
gowribalan123 <gowribalanak@gmail.com>
2025-02-13 14:10:01 +0530

    minor modifications

--- src/config/axiosInstance.jsx
+++ src/config/axiosInstance.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     baseURL: `${import.meta.env.VITE_API_URL}/api`,
- headers: {
-                'Content-Type': 'application/json',
-          },
+// headers: {
+    //            'Content-Type': 'application/json',
+    //      },
     withCredentials: true,
 });
