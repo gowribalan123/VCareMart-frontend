c50c83365adc785b6e853835a9001ccee72a7f48
gowribalan123 <gowribalanak@gmail.com>
2025-02-25 00:07:19 +0530

    minor modifications

--- src/layout/userLayout.jsx
+++ src/layout/userLayout.jsx
@@ -1,21 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { Header } from "../components/user/Header";
 import { Footer } from "../components/user/Footer";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation,useNavigate } from "react-router-dom";
 import { UserHeader } from "../components/user/UserHeader";
 import { axiosInstance } from "../config/axiosInstance";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch} from "react-redux";
 import { clearUser, saveUser } from "../redux/features/userSlice";
 
 export const UserLayout = () => {
     const { isUserAuth ,userData} = useSelector((state) => state.user);
     const dispatch = useDispatch();
     const location = useLocation();
+    const navigate = useNavigate();
     const [loading, setLoading] = useState(true);
 
     const checkUser = async () => {
         try {
-            const response = await axiosInstance.get("/user/check-user");
+            const response = await axiosInstance.get("/user/check-user",
+             {headers: { 
+                //Authorization: `Bearer ${token}`,
+                Authorization: `Bearer ${
+                   getTokenFromLocalStorage !== null ? getTokenFromLocalStorage.token : ""
+                 }`,
+                 'Content-Type': 'application/json',
+                 Accept: "application/json",
+                 
+            },
+
+
+                body: JSON.stringify({ status }),
+                withCredentials:true,
+                   credentials: "include"
+             }
+
+
+
+            );
             dispatch(saveUser(response.data));
         } catch (error) {
             dispatch(clearUser());
