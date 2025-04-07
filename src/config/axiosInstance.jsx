4722093188586c5d89a24cfa45ca30ddfc4d7864
gowribalan123 <gowribalanak@gmail.com>
2025-04-07 15:37:46 +0530

    minor modifications

--- src/config/axiosInstance.jsx
+++ src/config/axiosInstance.jsx
@@ -1,26 +1,31 @@
 import axios from "axios";
 
-
-const token = localStorage.getItem('authToken'); // Retrieve the token from local storage or your authentication logic
-
-//const getTokenFromLocalStorage = localStorage.getItem("user")
- // ? JSON.parse(localStorage.getItem("user"))
- ///: null;
-
-
+// Create an Axios instance
 export const axiosInstance = axios.create({
-
     baseURL: `${import.meta.env.VITE_API_URL}/api`,
-headers: { 
-             Authorization: `Bearer ${token}`,
-           //  Authorization: `Bearer ${
-             //  getTokenFromLocalStorage !== null ? getTokenFromLocalStorage.token : ""
-            // }`,
-              'Content-Type': 'application/json',
-             'Accept': 'application/json',
-              
-         },
-       ///  body: JSON.stringify({ status }),
-         withCredentials:true,
-           // credentials: "include"
+    headers: { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+    withCredentials: true,
+});
+
+// Interceptor to add the token to each request
+axiosInstance.interceptors.request.use(config => {
+    const token = localStorage.getItem('authToken'); // Retrieve the token before each request
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+
+// Interceptor to handle response errors globally
+axiosInstance.interceptors.response.use(response => {
+    return response;
+}, error => {
+    console.error("API call error:", error); // Log the error for debugging
+    // Optionally handle specific error cases here
+    return Promise.reject(error);
 });
